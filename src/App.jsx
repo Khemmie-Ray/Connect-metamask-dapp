9f7740f5d0d3469dffc122dcd8d983ce2f4833bb
Khemmie-Ray <atokemmy@gmail.com>
2023-01-26 21:46:42 +0100

    adding the connect wallet component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Wallet from './components/Wallet'
+
+function App() {
+  return (
+    <div>
+      <Wallet />
+    </div>
+  )
+}
+
+export default App
